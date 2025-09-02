b79f06b012e0045a710fc9595506bd848e21f87f
GenaroRafael <raphael12fa@gmail.com>
2025-09-01 21:16:02 -0600

    [FEAT:add] Utils: Creación de arquitectura del proyecto con modelos iniciales [#1]

---
+++ Utils/utils_cod/extensions/client/enums/client_type.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class ClientType(Enum):
+    STANDART: str = "standart"
+    STREAM: str = "stream"
+    VIP: str = "vip"
---
+++ Utils/utils_cod/extensions/plan/enums/plan_type.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class PlanType(Enum):
+    BASIC = "basic"
+    BASIC_STREAM = "basic_stream"
+    MEDIUM = "medium"
+    MEDIUM_STREAM = "medium_stream"
+    GEEK = "geek"
+    GEEK_STREAM = "geek_stream"
